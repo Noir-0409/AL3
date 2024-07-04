6ae34876fdf08d4003d10a7b810d7fdcdccf36c6
Noir-0409 <146914778+Noir-0409@users.noreply.github.com>
2024-07-04 12:42:40 +0900

    2_5終了

--- DirectXGame/Player.cpp
+++ DirectXGame/Player.cpp
@@ -137,22 +137,62 @@ void Player::Update() {
 
 		// 移動
 		worldTransform_.translation_ += velocity_;
-
+	
 		if (Input::GetInstance()->PushKey(DIK_UP)) {
 		
 		// ジャンプ初速
 			velocity_ += Vector3(0, kJumpAcceleration, 0);
-		
+	
+		}
+
+		// ジャンプ開始
+		if (velocity_.y > 0.0f) {
+
+			onGround_ = false;
+
 		}
 	
 // 空中
 	} else {
-	
-	// 落下速度
-		velocity_ += Vector3(0, -kGravityAcceleration, 0);
 
-		// 落下速度計算
+			// 落下速度
+			velocity_ += Vector3(0, -kGravityAcceleration, 0);
+
+		worldTransform_.translation_ += Vector3(velocity_);
+
+		// 落下速度制限
 		velocity_.y = std::max(velocity_.y, -kLimitFallSpeed);
+
+		// 着地フラグ
+		bool landing = false;
+
+		// 地面との当たり判定
+		// 下降中
+		if (velocity_.y < 0) {
+
+			// Y座標が地面以下になったら着地
+			if (worldTransform_.translation_.y <= 1.0f) {
+
+				landing = true;
+			}
+		}
+
+		// 着地
+		if (landing) {
+		
+		// めり込み排除
+			worldTransform_.translation_.y = 1.0f;
+
+			// 摩擦で横方向速度が減衰
+			velocity_.x *= (1.0f - kAttenuation);
+
+			// 下方向速度をリセット
+			velocity_.y = 0.0f;
+
+			// 接地状態に移行
+			onGround_ = true;
+		
+		}
 	
 	}
 
@@ -164,4 +204,5 @@ void Player::Draw() {
 
 	// 3Dモデルを描画
 	model_->Draw(worldTransform_, *viewProjection_, textureHandle_);
+
 }
--- DirectXGame/Player.h
+++ DirectXGame/Player.h
@@ -37,11 +37,14 @@ private:
 
 	Vector3 velocity_ = {};
 
+	// 加速度
 	static inline const float kAcceleration = 0.1f;
 
+	// 速度減衰率
 	static inline const float kAttenuation = 0.1f;
 
-	static inline const float kLimitRunSpeed = 3.0f;
+	// 最大速度
+	static inline const float kLimitRunSpeed = 1.0f;
 
 	// 左右
 	enum class LRDirection {
@@ -66,11 +69,11 @@ private:
 	bool onGround_ = true;
 
 	// 重力加速度
-	static inline const float kGravityAcceleration = 9.8f;
+	static inline const float kGravityAcceleration = 0.05f;
 
 	// 最大落下速度
-	static inline const float kLimitFallSpeed = 5.0f; 
+	static inline const float kLimitFallSpeed = 1.0f; 
 
 	// ジャンプ初速
-	static inline const float kJumpAcceleration = 5.0f;
+	static inline const float kJumpAcceleration = 1.0f;
 };
