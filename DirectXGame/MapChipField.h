be5f0ab49a7fd544004c9952153a6ea50a673155
Noir-0409 <146914778+Noir-0409@users.noreply.github.com>
2024-07-25 12:15:29 +0900

    27枚目まで

--- DirectXGame/MapChipField.h
+++ DirectXGame/MapChipField.h
@@ -16,21 +16,18 @@ struct MapChipData {
 
 };
 
+struct IndexSet {
+
+	uint32_t xIndex;
+	uint32_t yIndex;
+
+};
+
 class MapChipField {
 
 	public:
 
-	// 1ブロックのサイズ
-	static inline const float kBlockWidth = 2.0f;
-	static inline const float kBlockHeight = 2.0f;
-
-	// ブロックの個数
-	static inline const uint32_t kNumBlockVirtical = 20;
-	static inline const uint32_t kNumBlockHorizontal = 100;
-
 	MapChipData mapChipData_;
-
-	
 
 	void ResetMapChipData();
 
@@ -43,7 +40,17 @@ class MapChipField {
 	auto GetNumBlockVirical() { return kNumBlockVirtical; }
 	auto GetNumBlockHorizontal() {return kNumBlockHorizontal;}
 
+	IndexSet GetMapChipIndexSetByPosition(const Vector3& position);
+
 	private:
+
+		// 1ブロックのサイズ
+	    static inline const float kBlockWidth = 2.0f;
+	    static inline const float kBlockHeight = 2.0f;
+
+	    // ブロックの個数
+	    static inline const uint32_t kNumBlockVirtical = 20;
+	    static inline const uint32_t kNumBlockHorizontal = 100;
 
 };
 
