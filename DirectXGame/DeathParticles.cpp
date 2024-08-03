bd731e4e8a8c5ada6154d1815eac0ac029fda06e
Noir-0409 <146914778+Noir-0409@users.noreply.github.com>
2024-08-03 22:21:23 +0900

    13枚目まで

---
+++ DirectXGame/DeathParticles.cpp
@@ -0,0 +1,42 @@
+#include "DeathParticles.h"
+
+void DeathParticle::Initialize(
+    Model* model, ViewProjection* viewProjection, const Vector3& position) {
+
+   particleModel_ = model;
+
+   viewProjection_ = viewProjection;
+
+   // ワールド変換の初期化
+   for (auto& worldTransform : worldTransforms_) {
+   
+   worldTransform.Initialize();
+
+   worldTransform.translation_ = position;
+   
+   }
+
+}
+
+void DeathParticle::Update() {
+
+    // ワールド座標の更新
+   for (auto& worldTransform : worldTransforms_) {
+   
+   worldTransform.UpdateMatrix();
+   
+   }
+}
+
+void DeathParticle::Draw() {
+
+// モデルの描画
+   for (const auto& worldTransform : worldTransforms_) {
+
+   particleModel_->Draw(worldTransform, *viewProjection_);
+
+   }
+
+  
+
+}
---
+++ DirectXGame/DeathParticles.h
@@ -0,0 +1,33 @@
+#include <Model.h>
+#include <array>
+
+class DeathParticle {
+
+public:
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize(Model* model, ViewProjection* viewProjection, const Vector3& position);
+
+	/// <summary>
+	/// 更新
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	void Draw();
+
+	// パーティクルの個数
+	static inline const uint32_t kNumParticles = 8;
+
+	std::array<WorldTransform, kNumParticles> worldTransforms_;
+
+	private:
+
+	Model* particleModel_ = nullptr;           // パーティクルのモデル
+    ViewProjection* viewProjection_ = nullptr; // ビュー・プロジェクションデータ
+
+};
