33c756d34c59d9f2f1bdc2021f97020bf8ed60d6
Noir-0409 <146914778+Noir-0409@users.noreply.github.com>
2024-08-27 00:47:44 +0900

    GetAABBまで

--- DirectXGame/Player.h
+++ DirectXGame/Player.h
@@ -3,6 +3,7 @@
 #include "WorldTransform.h"
 #include "MapChipField.h"
 #include "MathUtilityForText.h"
+#include "AB.h"
 
 /// <summary>
 /// 自キャラ
@@ -88,6 +89,12 @@ public:
 
 	// 壁に接触している場合の処理
 	void UpdateHitWall(const CollisionMapInfo& info);
+
+	// ワールド座標を取得
+	Vector3 GetWorldPosition();
+
+	// AABBを取得
+	AABB GetAABB();
 
 private:
 	// ワールド変換データ
